4977ec1b8caf4aa6f7af1827293393dbc67fe1cd
vloparevich <vadim.loparevich@gmail.com>
2021-07-31 14:44:55 -0700

    Rename the func

--- two-to-one.js
+++ two-to-one.js
@@ -1,4 +1,4 @@
-function longest(s1, s2) {
+function twoToOne(s1, s2) {
     const lettersSortedArr = [...s1, ...s2].sort();
     lettersSortedArr.forEach(letter => {
         if (result[result.length - 1] !== letter) {
